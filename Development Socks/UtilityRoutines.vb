25ac45da2940fc5b63660658345e4eb730891086
rattwood <rattwood@tecknoserve.com>
2018-05-31 15:16:24 +0700

    Change to spash screen for V45

---
+++ Development Socks/UtilityRoutines.vb
@@ -0,0 +1,12 @@
+﻿Module UtilityRoutines
+    Public Sub ReleaseComObject(ByVal obj As Object)
+        Try
+            If obj IsNot Nothing Then
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+                obj = Nothing
+            End If
+        Catch ex As Exception
+            obj = Nothing
+        End Try
+    End Sub
+End Module
