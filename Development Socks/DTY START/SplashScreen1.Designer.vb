1c89a5dd9635721ae93dc2d7bae305e5be799887
rattwood <rattwood@tecknoserve.com>
2019-04-09 11:09:51 +0700

    Release 84  with fix for Cart report

--- Development Socks/DTY START/SplashScreen1.Designer.vb
+++ Development Socks/DTY START/SplashScreen1.Designer.vb
@@ -78,7 +78,7 @@ Partial Class SplashScreen1
         Me.Version.Name = "Version"
         Me.Version.Size = New System.Drawing.Size(241, 37)
         Me.Version.TabIndex = 1
-        Me.Version.Text = "Version 2.0.0.83 support@tecknoserve.com"
+        Me.Version.Text = "Version 2.0.0.84 support@tecknoserve.com"
         '
         'Copyright
         '
