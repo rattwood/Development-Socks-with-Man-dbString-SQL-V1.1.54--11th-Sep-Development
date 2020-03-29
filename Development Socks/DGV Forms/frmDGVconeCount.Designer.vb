09f0e06bcc27a7e6f6487c48b33e53ffdd9c07cc
rattwood <rattwood@tecknoserve.com>
2020-03-29 08:01:23 +0700

    Added new dedicated DGV

---
+++ Development Socks/DGV Forms/frmDGVconeCount.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDGVconeCount
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DGVconeCount = New System.Windows.Forms.DataGridView()
+        CType(Me.DGVconeCount, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DGVconeCount
+        '
+        Me.DGVconeCount.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGVconeCount.Location = New System.Drawing.Point(77, 59)
+        Me.DGVconeCount.Name = "DGVconeCount"
+        Me.DGVconeCount.Size = New System.Drawing.Size(240, 150)
+        Me.DGVconeCount.TabIndex = 0
+        '
+        'frmDGVconeCount
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(342, 247)
+        Me.Controls.Add(Me.DGVconeCount)
+        Me.Name = "frmDGVconeCount"
+        Me.Text = "frmDGVconeCount"
+        CType(Me.DGVconeCount, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DGVconeCount As DataGridView
+End Class
---
+++ Development Socks/DGV Forms/frmDGVconeCount.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmDGVconeCount
+
+End Class
